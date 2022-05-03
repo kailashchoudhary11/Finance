27bba3dbfdc502267b9672ab619acc4982798d23
cobra <kainewhere@gmail.com>
2022-05-03 10:56:54 +0530

    static files

--- finance/settings.py
+++ finance/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'home/static/home'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
