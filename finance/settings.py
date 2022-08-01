430e0925be569f1c5f0784fabafbe777554939c8
kai <kailashjat604@gmail.com>
2022-08-01 07:05:31 +0530

    Add Database url

--- finance/settings.py
+++ finance/settings.py
@@ -93,7 +93,7 @@ if DEBUG == True:
     }
 else :
     DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-    DATABASES['default'] = dj_database_url.config(default='postgres://...')
+    DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
